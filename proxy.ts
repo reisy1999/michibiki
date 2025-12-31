cc1dbe9d69a9872d28de1834d72342bbcb16c8da
reisy1999 <abereneibu@gmail.com>
2025-12-31 20:32:38 +0900

    fix: proxy.ts Next.js 15から middleware.ts の名前が変更 Next.js 15から middleware.ts の名前が変わりました。 > mv middleware.ts proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,6 @@
 //Auth.js　https://authjs.dev/getting-started/installation
 //セッションを維持するためのオプションのミドルウェアを追加します。
 //これにより、呼び出されるたびにセッションの有効期限が更新されます。
+//Next.js 15から middleware.ts の名前がproxy.tsに変更されました。
 
 export { auth as middleware } from "@/lib/auth"
