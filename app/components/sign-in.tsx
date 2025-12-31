d3815d41c09085ab3eb3436ebff6bea7e741a91b
reisy1999 <abereneibu@gmail.com>
2025-12-31 20:20:02 +0900

    feat: app/components/sign-in.tsx サインインページサンプル https://authjs.dev/getting-started/authentication/oauth

---
+++ app/components/sign-in.tsx
@@ -0,0 +1,17 @@
+//Auth.js https://authjs.dev/getting-started/authentication/oauth
+//サインインボタンexample
+
+import { signIn } from "@/lib/auth"
+ 
+export default function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server"
+        await signIn("google")
+      }}
+    >
+      <button type="submit">Signin with Google</button>
+    </form>
+  )
+} 
