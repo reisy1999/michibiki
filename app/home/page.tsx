8ea2c0f3db7358335d4eb9a37efa219e8a353b6d
reisy1999 <abereneibu@gmail.com>
2026-01-01 14:00:45 +0900

    app/home/page.tsx consoleにsession表示機能追加test段階

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation"
 
 export default async function HomePage() {
   const session = await auth()
+  console.log("session:", JSON.stringify(session, null, 2))
   
   // ログインしてなければトップへ
   if (!session) {
@@ -13,7 +14,15 @@ export default async function HomePage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="text-center">
-        <h1 className="text-2xl font-bold mb-4">ログイン成功！</h1>
+        <h1 className="text-2xl font-bold mb-4">{session.user?.name}</h1>
+        <h2>
+          <ul>
+            <li>{session.user?.email}</li>
+            <li>{session.user?.image}</li>
+            <li>{session.user?.id}</li>
+            <li>{session.expires}</li>
+          </ul>
+        </h2>
         <SignOut />
       </div>
     </div>
