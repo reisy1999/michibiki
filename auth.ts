41d10945e10aa67f1faa0e773d0657df5dd3e991
reisy1999 <abereneibu@gmail.com>
2026-01-01 23:34:52 +0900

    feat: auth.ts idをcallbackで参照できるように実装

--- auth.ts
+++ auth.ts
@@ -6,4 +6,18 @@ import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-})
+    //  By default, the `id` property does not exist on `token` or `session`. See the [TypeScript](https://authjs.dev/getting-started/typescript) on how to add it.
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) { // User is available during sign-in
+        token.id = user.id
+      }
+      return token
+    },
+    session({ session, token }) {
+      session.user.id = token.id
+      return session
+    },
+  },
+}
+)
