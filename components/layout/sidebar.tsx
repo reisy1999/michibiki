17ebadf168fbf03cc02e065f916b3abf533cfbef
reisy1999 <abereneibu@gmail.com>
2026-01-11 23:58:26 +0900

    feat: layout/side.bar shadcn対応のsidebar実装

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,10 +1,20 @@
-export const Sidebar = () => {
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+
+export function AppSidebar() {
   return (
-    <aside className="w-64 h-screen bg-white-500">
-      <div className="mb-8 p-4">
-        <h2 className="text-xl font-bold text-gray-800">michibiki</h2>
-        <h2 className="text-2xl font-bold mb-4">aa</h2>
-      </div>
-    </aside>
-  );
-};
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
