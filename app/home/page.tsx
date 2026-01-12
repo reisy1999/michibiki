066ffedf001494e4dc1ea2768a60d421e35c3ba4
reisy1999 <abereneibu@gmail.com>
2026-01-12 21:58:22 +0900

    feat: home/page.tsx なにもないメイン画面実装

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,7 @@
 //ログイン後最初のページ
+import * as React from "react"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator"
 import { auth } from "@/lib/auth";
 import { SignOut } from "@/components/auth/sign-out";
 import { redirect } from "next/navigation";
@@ -11,21 +14,10 @@ export default async function HomePage() {
     redirect("/");
   }
   return (
-    <div className="flex min-h-screen">
-      <div className="flex-1 flex items-center justify-center">
-        <div className="text-center">
-          <h1 className="text-2xl font-bold mb-4">{session.user?.name}</h1>
-          <h2>
-            <ul>
-              <li>{session.user?.email}</li>
-              <li>{session.user?.image}</li>
-              <li>{session.user?.id}</li>
-              <li>{session.expires}</li>
-            </ul>
-          </h2>
-          <SignOut />
-        </div>
+    <ScrollArea className="flex flex-cal h-screen">
+      <div>
+        <h1>なにもない！</h1>
       </div>
-    </div>
-  );
+    </ScrollArea>
+  )
 }
